8f49513c157de7511d6ae879a8de031ba875dc3e
xahram <ssasuke1163@gmail.com>
2020-03-04 22:13:00 +0500

    Added require for mongoose of db in app file, also changed localhost to 127.0.0.1

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+require('../db/mongoose')
 const User = require('../model/users')
 
 app.use(express.json())
 
 app.post('/users', async (req, res) => {
     const user = new User({ username: req.body.username, email: req.body.email, password: req.body.password })
+    console.log(req.body, user)
     try {
         await user.save()
-        res.send(user)
+        res.status(201).send(user)
     } catch (e) {
         throw new Error(e)
     }
